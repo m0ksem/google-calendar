4e3195868f1aba679805be469ef8dc059d15fdc9
m0ksem <m0ksem1337@gmail.com>
2021-11-05 22:34:25 +0200

    Init google calendar sample.

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,70 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <button @click="login">Login</button>
+    <button @click="loadEvents">Load</button>
+    <button @click="createSimpleEvent">createSimpleEvent</button>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
+
+  beforeCreate() {
+    this.$gapi.getGapiClient()
+  },
+
+  methods: {
+    async login() {
+      await this.$gapi.login()
+    },
+
+    async loadEvents() {
+      const client = (await this.$gapi.getGapiClient()).client
+
+      const response = await client.calendar.events.list({
+        calendarId: 'primary',
+        timeMin: new Date().toISOString(),
+        showDeleted: false,
+        singleEvents: true,
+        maxResults: 10,
+        orderBy: 'startTime'
+      })
+
+      console.log(JSON.parse(response.body))
+    },
+
+    async createSimpleEvent() {
+      const client = (await this.$gapi.getGapiClient()).client
+
+      await client.calendar.events.insert({
+        calendarId: 'primary',
+        resource: {
+          'summary': 'Test',
+          'location': '800 Howard St., San Francisco, CA 94103',
+          'description': 'A chance to hear more about Google\'s developer products.',
+          'start': {
+            'dateTime': new Date().toISOString(),
+            'timeZone': Intl.DateTimeFormat().resolvedOptions().timeZone
+          },
+          'end': {
+            'dateTime': new Date().toISOString(),
+            'timeZone': Intl.DateTimeFormat().resolvedOptions().timeZone
+          },
+          'recurrence': [
+            'RRULE:FREQ=DAILY;COUNT=2'
+          ],
+          'reminders': {
+            'useDefault': false,
+            'overrides': [
+              {'method': 'email', 'minutes': 24 * 60},
+              {'method': 'popup', 'minutes': 10}
+            ]
+          }
+        }
+      })
+    }
+  },
 }
 </script>
 
--- src/main.js
+++ src/main.js
@@ -1,7 +1,15 @@
 import Vue from 'vue'
 import App from './App.vue'
+import GApi from 'vue-gapi'
 
 Vue.config.productionTip = false
+
+Vue.use(GApi, {
+  apiKey: 'AIzaSyDPzLY6L36krINVPj-76dGFFKMX9w69au4',
+  clientId: '804955345322-7vko1g67u1lmt317dqfchh24b2rn1d3d.apps.googleusercontent.com',
+  discoveryDocs: ['https://www.googleapis.com/discovery/v1/apis/calendar/v3/rest'],
+  scope: 'https://www.googleapis.com/auth/calendar',
+})
 
 new Vue({
   render: h => h(App),
