013590d258649e5d71fe2f6ef2bc8d5e5c7c125d
Darya Volkova <anrich@bk.ru>
2021-11-09 17:04:53 +0200

    Updated button

--- src/components/MyButton.vue
+++ src/components/MyButton.vue
@@ -5,9 +5,17 @@
 <style lang="scss" scoped>
 button {
   border-radius: 9px;
-  padding: 5px 20px;
+  padding: 10px 30px;
   height: 100%;
-  background-color: violet;
+  background: #995dd1;
+  color: white;
+  font-weight: 500;
+  cursor: pointer;
+  border: 1px;
+  transition: all 0.2s ease-in-out;
 
+  &:hover {
+background: #b07fdd;
+  }
 }
 </style>
